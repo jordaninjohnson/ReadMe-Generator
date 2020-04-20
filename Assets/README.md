52f11bf3f71c8926868aa17818767037e2ce77e5
Jordan Johnson <jordaninjohnson@gmail.com>
2020-04-19 19:16:27 -0600

    titles in order

--- Assets/README.md
+++ Assets/README.md
@@ -1,4 +1,18 @@
-<h1>title</h1>
+<h1>Enter Title</h1>
 
-badge:badge
-Username:username
+<h1>Description</h1>
+
+<h1>Table of Contents</h1>
+
+<h1>Installation</h1>
+
+<h1>Usage</h1>
+
+<h1>License</h1>
+
+<h1>Contributing</h1>
+
+<h1>Tests</h1>
+
+<h1>Questions</h1>
+
--- index.js
+++ index.js
@@ -16,6 +16,46 @@ inquirer
             default: 'Enter Title',
         },
         {
+            name: 'Description',
+            message: 'What is the description of your project?',
+            default: 'Enter description',
+        },
+        {
+            name: 'Table of Contents',
+            message: 'What is Table of Contents?',
+            default: '',
+        },
+        {
+            name: 'Installation',
+            message: 'How do you install your project?',
+            default: '',
+        },
+        {
+            name: 'Usage',
+            message: 'How do you use your project?',
+            default: '',
+        },
+        {
+            name: 'License',
+            message: 'What license does your project hold?',
+            default: '',
+        },
+        {
+            name: 'Contributing',
+            message: 'How to contribute?',
+            default: '',
+        },
+        {
+            name: 'Tests',
+            message: 'What tests have you performed?',
+            default: '',
+        },
+        {
+            name: 'Questions',
+            message: 'Common questions?',
+            default: '',
+        },
+        {
             name: 'GitHubUsername',
             message: 'What is your GitHub Username?',
             default: 'Enter your GitHub Username!',
@@ -28,20 +68,64 @@ inquirer
     ])
     .then(answers => {
         //console.info('Answers:', answers);
+        //Title
+        //creates a variable for title that is <h1> in MD
         var projectTitle = md.render('# ' + answers.Title);
-        //FS
-        fs.appendFile('./Assets/README.md', 'badge:' + answers.badge + '\n', function (err) {
-            if (err) throw err;
-            console.log('badge Saved!');
-        });
-        fs.appendFile('./Assets/README.md', 'Username:' + answers.GitHubUsername + '\n', function (err) {
-            if (err) throw err;
-            console.log('Username Saved!');
-        });
-        fs.appendFile('./Assets/README.md', projectTitle + '\n', function (err) {
+        fs.appendFileSync('./Assets/README.md', projectTitle + '\n', function (err) {
             if (err) throw err;
             console.log('title saved');
         })
+        //Description
+        var descriptionTitle = md.render('# Description');
+        fs.appendFileSync('./Assets/README.md', descriptionTitle + '\n', function (err) {
+            if (err) throw err;
+        })
+        //Table of Contents
+        var tableOfContentsTitle = md.render('# Table of Contents');
+        fs.appendFileSync('./Assets/README.md', tableOfContentsTitle + '\n', function (err) {
+            if (err) throw err;
+        })
+        //Installation
+        var installationTitle = md.render('# Installation');
+        fs.appendFileSync('./Assets/README.md', installationTitle + '\n', function (err) {
+            if (err) throw err;
+        })
+        //Usage
+        var usageTitle = md.render('# Usage');
+        fs.appendFileSync('./Assets/README.md', usageTitle + '\n', function (err) {
+            if (err) throw err;
+        })
+        //License
+        var licenseTitle = md.render('# License');
+        fs.appendFileSync('./Assets/README.md', licenseTitle + '\n', function (err) {
+            if (err) throw err;
+        })
+        //Contributing
+        var contributingTitle = md.render('# Contributing');
+        fs.appendFileSync('./Assets/README.md', contributingTitle + '\n', function (err) {
+            if (err) throw err;
+        })
+        //Tests
+        var testsTitle = md.render('# Tests');
+        fs.appendFileSync('./Assets/README.md', testsTitle + '\n', function (err) {
+            if (err) throw err;
+        })
+        //Questions
+        var questionsTitle = md.render('# Questions');
+        fs.appendFileSync('./Assets/README.md', questionsTitle + '\n', function (err) {
+            if (err) throw err;
+        })
+
+
+        // GitHub Username & Badge
+        // fs.appendFile('./Assets/README.md', 'Username:' + answers.GitHubUsername + '\n', function (err) {
+        //     if (err) throw err;
+        //     console.log('Username Saved!');
+        // });
+        // fs.appendFile('./Assets/README.md', 'badge:' + answers.badge + '\n', function (err) {
+        //     if (err) throw err;
+        //     console.log('badge Saved!');
+        // });
     });
 
 
